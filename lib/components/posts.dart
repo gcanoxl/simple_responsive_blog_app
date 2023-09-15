5254061f6493688c481cf589e2d9ff2025d284f6
Cano XL <gcanoxl@gmail.com>
2023-09-16 02:17:03 +0800

    add blogs

---
+++ lib/components/posts.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:simple_fastapi_blog_app/constants.dart';
+import 'package:simple_fastapi_blog_app/models/post.dart';
+import 'package:simple_fastapi_blog_app/providers/post_provider.dart';
+
+class Posts extends StatefulWidget {
+  const Posts({
+    super.key,
+  });
+
+  @override
+  State<Posts> createState() => _PostsState();
+}
+
+class _PostsState extends State<Posts> {
+  final List<Post> posts = [];
+
+  @override
+  void initState() {
+    var data = PostProvider().getPosts(10);
+    data.then((value) {
+      setState(() {
+        posts.addAll(value.body!);
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: List.generate(
+        posts.length,
+        (index) => PostItem(post: posts[index]),
+      ),
+    );
+  }
+}
+
+class PostItem extends StatelessWidget {
+  const PostItem({
+    super.key,
+    required this.post,
+  });
+  final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    var color = Theme.of(context).colorScheme.inversePrimary;
+    return Container(
+      margin: const EdgeInsets.only(top: kDefaultPadding),
+      padding: const EdgeInsets.all(kDefaultPadding),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: color.withOpacity(0.1),
+        // border: Border(bottom: BorderSide(width: 1, color: color))
+        // borderRadius: const BorderRadius.all(
+        //   Radius.circular(kDefaultPadding / 3),
+        // ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            post.title,
+            style: const TextStyle(fontSize: 22),
+          ),
+          const SizedBox(height: kDefaultPadding / 2),
+          Text(
+            "Created At: 2022-02-29",
+            style: TextStyle(
+              color: Colors.black.withOpacity(0.4),
+            ),
+          ),
+          const SizedBox(height: kDefaultPadding),
+          Text(
+            post.content,
+            style: const TextStyle(fontSize: 16),
+          ),
+          const SizedBox(height: kDefaultPadding / 2),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                "Views: ${post.views}",
+                style: TextStyle(color: Colors.black.withOpacity(0.4)),
+              ),
+              const Spacer(),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.comment),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.add_comment),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.thumb_up),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/routes/home_screen.dart
+++ lib/routes/home_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:simple_fastapi_blog_app/components/nav_bar.dart';
+import 'package:simple_fastapi_blog_app/components/posts.dart';
 import 'package:simple_fastapi_blog_app/constants.dart';
-import 'package:simple_fastapi_blog_app/models/index.dart';
-import 'package:simple_fastapi_blog_app/providers/post_provider.dart';
-import '../components/nav_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,81 +10,32 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: [
-              const NavBar(),
-              Container(
-                constraints: const BoxConstraints(maxWidth: kMaxWidth),
-                child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      //Left
-                      Posts(),
-                      //Right
-                      Column(
-                        children: [],
-                      ),
-                    ]),
+        child: Column(
+          children: [
+            const NavBar(),
+            Container(
+              constraints: const BoxConstraints(maxWidth: kMaxWidth),
+              child: const Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //Left
+                  Expanded(
+                    flex: 2,
+                    child: Posts(),
+                  ),
+                  //Right
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      children: [],
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
-    );
-  }
-}
-
-class Posts extends StatefulWidget {
-  const Posts({
-    super.key,
-  });
-
-  @override
-  State<Posts> createState() => _PostsState();
-}
-
-class _PostsState extends State<Posts> {
-  final List<Post> posts = [];
-
-  @override
-  void initState() {
-    var data = PostProvider().getPosts(10);
-    data.then((value) {
-      setState(() {
-        posts.addAll(value.body!);
-      });
-    });
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: List.generate(
-        posts.length,
-        (index) => PostItem(post: posts[index]),
-      ),
-    );
-  }
-}
-
-class PostItem extends StatelessWidget {
-  const PostItem({
-    super.key,
-    required this.post,
-  });
-  final Post post;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Text(post.title),
-        Text(post.content),
-      ],
     );
   }
 }
